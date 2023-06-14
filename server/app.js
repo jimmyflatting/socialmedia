eca775a2ef3c03186ad76f49b022660f8e948288
Jimmy Flatting <jimmy.flatting@icloud.com>
2023-06-14 20:35:53 +0200

    cors origin

--- server/app.js
+++ server/app.js
@@ -16,7 +16,7 @@ const profile = require('./routes/profile');
 
 app.use(cors());
 app.use(express.json());
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors());
 app.use(cookieParser());
 app.use(bodyParser.json());
 
