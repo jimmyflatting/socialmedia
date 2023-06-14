f2424798a79d1609f9db76af34bab172351f6171
Jimmy Flatting <jimmy.flatting@icloud.com>
2023-06-14 21:18:00 +0200

    giga logs

--- server/routes/session.js
+++ server/routes/session.js
@@ -4,11 +4,13 @@ const jwt = require('jsonwebtoken');
 
 router.get('/', (req, res) => {
 	const token = req.cookies.token;
+	console.log('Token:', token); // Add this line to log the token value
 	if (token) {
-		console.log('Token:', token);
+		console.log('Before verification'); // Add this line to check the flow
 		jwt.verify(token, process.env.TOKEN_KEY, (err) => {
+			console.log('After verification'); // Add this line to check the flow
 			if (err) {
-				console.log('Verification Error:', err);
+				console.log('Verification Error:', err); // Add this line to log the error
 				res.status(401).json({ message: err });
 			} else {
 				res.sendStatus(200);
