b877f390f11548ea422245cb1c3055ec06b08c72
Jimmy Flatting <jimmy.flatting@icloud.com>
2023-06-24 23:23:42 +0200

    fix validate

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,5 @@
-import React, {
-	useEffect,
-} from "react";
-import {
-	Route,
-	Routes,
-	useNavigate,
-} from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Feed from "./pages/Feed";
 import Profile from "./pages/Profile";
@@ -14,62 +8,46 @@ import { config } from "./utils/config";
 const apiUrl = config.API_BASE_URL;
 
 function App() {
-	const navigate = useNavigate();
+  const [validate, setValidate] = useState(false);
+  const navigate = useNavigate();
 
-	useEffect(() => {
-		const fetchData = async () => {
-			try {
-				const response =
-					await fetch(
-						`${apiUrl}auth/validate`,
-						{
-							method: "GET",
-							headers: {
-								"Content-Type":
-									"application/json",
-							},
-							credentials:
-								"include",
-						}
-					);
-				// Glöm ej fixa så /:id inte redirectar till home
-				response.status === 200
-					? navigate("/feed")
-					: navigate("/");
-			} catch (err) {
-				console.log(err);
-			}
-		};
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${apiUrl}auth/validate`, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          credentials: "include",
+        });
+        if (response.status === 200) {
+          setValidate(true);
+        } else {
+          setValidate(false);
+          navigate("/");
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    };
 
-		fetchData();
-	}, [navigate]);
+    fetchData();
+  }, [navigate]);
 
-	return (
-		<>
-			<div className="app bg-gray-100 dark:bg-gray-900">
-				<Routes>
-					<Route
-						path="/"
-						element={
-							<Home />
-						}
-					/>
-					<Route
-						path="/feed"
-						element={
-							<Feed />
-						}
-					/>
-					<Route
-						path="/:id"
-						element={
-							<Profile />
-						}
-					/>
-				</Routes>
-			</div>
-		</>
-	);
+  return (
+    <div className="app bg-gray-100 dark:bg-gray-900">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {validate ? (
+          <>
+            <Route path="/feed" element={<Feed />} />
+            <Route path="/:id" element={<Profile />} />
+          </>
+        ) : null}
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
