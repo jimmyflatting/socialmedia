c4edd796ddcbf7ba320fb2795e515b5e52f9d627
Jimmy Flatting <jimmy@Jimmys-MacBook-Air.local>
2023-06-26 08:11:45 +0200

    prettier setup

--- client-old/src/views/Feed.jsx
+++ client-old/src/views/Feed.jsx
@@ -1,55 +1,55 @@
-import React, { useEffect, useState } from 'react';
-import FeedComponent from '../components/feed/FeedComponent';
-import ProfileComponent from '../components/profile/ProfileComponent';
-import FriendList from '../components/profile/FriendList';
-import Header from '../components/navigation/Header';
-import { config } from '../utils/config';
+import React, { useEffect, useState } from "react";
+import FeedComponent from "../components/feed/FeedComponent";
+import ProfileComponent from "../components/profile/ProfileComponent";
+import FriendList from "../components/profile/FriendList";
+import Header from "../components/navigation/Header";
+import { config } from "../utils/config";
 const apiUrl = config.API_BASE_URL;
 
 const Feed = () => {
-	const [userData, setUserData] = useState(null);
+  const [userData, setUserData] = useState(null);
 
-	useEffect(() => {
-		const fetchUserData = async () => {
-			try {
-				const response = await fetch(`${apiUrl}users/`, {
-					headers: {
-						'Content-Type': 'application/json',
-					},
-					credentials: 'include',
-				});
-				const data = await response.json();
-				// console.log(data);
-				setUserData(data);
-			} catch (error) {
-				console.log(error);
-			}
-		};
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const response = await fetch(`${apiUrl}users/`, {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          credentials: "include",
+        });
+        const data = await response.json();
+        // console.log(data);
+        setUserData(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
-		fetchUserData();
-	}, []);
+    fetchUserData();
+  }, []);
 
-	if (!userData) {
-		return <div>Loading...</div>;
-	}
-	return (
-		<>
-			<div className='container'>
-				<Header user={userData} />
-				<div className='row'>
-					<div className='col-12 col-xl-3'>
-						<ProfileComponent user={userData} />
-					</div>
-					<div className='col-12 col-xl-6'>
-						<FeedComponent user={userData} />
-					</div>
-					<div className='col-3 d-none d-xl-block'>
-						<FriendList user={userData} />
-					</div>
-				</div>
-			</div>
-		</>
-	);
+  if (!userData) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <>
+      <div className="container">
+        <Header user={userData} />
+        <div className="row">
+          <div className="col-12 col-xl-3">
+            <ProfileComponent user={userData} />
+          </div>
+          <div className="col-12 col-xl-6">
+            <FeedComponent user={userData} />
+          </div>
+          <div className="col-3 d-none d-xl-block">
+            <FriendList user={userData} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Feed;
--- client-old/src/views/Login.jsx
+++ client-old/src/views/Login.jsx
@@ -1,20 +1,21 @@
-import React from 'react';
-import LoginComponent from '../components/auth/LoginComponent';
+import React from "react";
+import LoginComponent from "../components/auth/LoginComponent";
 
 const Login = () => {
-	return (
-		<div
-			className='container vh-100 d-flex align-items-center justify-content-center'
-			style={{ minHeight: '100vh' }}>
-			<div className='col-12'>
-				<div className='row justify-content-center'>
-					<div className='col-12 col-md-6 col-lg-6'>
-						<LoginComponent />
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div
+      className="container vh-100 d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <div className="col-12">
+        <div className="row justify-content-center">
+          <div className="col-12 col-md-6 col-lg-6">
+            <LoginComponent />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
